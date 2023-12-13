838d69be5567d5c5076ec0f5ccd3d5f6fca20f0f
Supermarcel10 <marcelbarlik@gmail.com>
2023-12-13 20:33:10 +0000

    Day 3 // Task 2
    Took 1 hour 30 minutes

--- src/Day3/Main.java
+++ src/Day3/Main.java
@@ -2,8 +2,7 @@ package Day3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,6 +22,7 @@ public class Main {
 
 			String[] linesArray = lines.toArray(new String[0]);
 			System.out.println("Sum of part numbers: " + sumPartNumbers(linesArray));
+			System.out.println("Sum of gear ratios: " + sumGearRatios(linesArray));
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found!");
 		}
@@ -77,5 +77,68 @@ public class Main {
 			return ' ';
 		}
 		return schematic[row].charAt(column);
+	}
+
+	public static int sumGearRatios(String[] schematic) {
+		int sum = 0;
+
+		for (int row = 0; row < schematic.length; row++) {
+			for (int col = 0; col < schematic[row].length(); col++) {
+				if (schematic[row].charAt(col) == '*') {
+					sum += lookAround(schematic, row, col);
+				}
+			}
+		}
+
+		return sum;
+	}
+
+	public static int lookAround(String[] schematic, int row, int col) {
+		List<Integer> foundNumbers = new ArrayList<>();
+		Set<String> countedNumbersWithPositions = new HashSet<>();
+
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (i == 0 && j == 0) continue; // Skip itself
+
+				int newRow = row + i;
+				int newCol = col + j;
+
+				if (newRow >= 0 && newRow < schematic.length && newCol >= 0 && newCol < schematic[0].length()) {
+					char character = schematic[newRow].charAt(newCol);
+					if (Character.isDigit(character)) {
+						String fullNumber = findFullNumber(schematic, newRow, newCol);
+						int startCol = newCol;
+						while (startCol > 0 && Character.isDigit(schematic[newRow].charAt(startCol - 1))) {
+							startCol--;
+						}
+						String uniqueIdentifier = fullNumber + "@" + newRow + "," + startCol;
+						if (!countedNumbersWithPositions.contains(uniqueIdentifier)) {
+							foundNumbers.add(Integer.parseInt(fullNumber));
+							countedNumbersWithPositions.add(uniqueIdentifier);
+						}
+					}
+				}
+			}
+		}
+
+		return foundNumbers.size() == 2 ? foundNumbers.get(0) * foundNumbers.get(1) : 0;
+	}
+
+	public static String findFullNumber(String[] schematic, int row, int col) {
+		StringBuilder number = new StringBuilder();
+		int lineLength = schematic[0].length();
+		int startColumn = col;
+
+		while (startColumn > 0 && Character.isDigit(schematic[row].charAt(startColumn - 1))) {
+			startColumn--;
+		}
+
+		while (startColumn < lineLength && Character.isDigit(schematic[row].charAt(startColumn))) {
+			number.append(schematic[row].charAt(startColumn));
+			startColumn++;
+		}
+
+		return number.toString();
 	}
 }
