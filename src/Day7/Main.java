e8d778da30e7556c3cedd561473ed91d40104c33
marcel <marcelbarlik@gmail.com>
2023-12-14 09:12:36 +0000

    Prep for Day 5-10

---
+++ src/Day7/Main.java
@@ -0,0 +1,27 @@
+package Day7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("src/Day5/input.txt");
+            Scanner scanner = new Scanner(file);
+            ArrayList<String> lines = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+
+            scanner.close();
+
+            // TODO: Implement solution here
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+}
